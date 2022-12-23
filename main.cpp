e0f7e8791dc61d75382e5eb63951b2f9a465e3a4
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2022-12-23 15:48:15 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,643 @@
+#include<string>
+#include<iostream>
+#include<vector>
+
+/*
+* The syntax:
+* 
+* A function call:
+* 
+* function_call function_name<>!;
+* 
+* A class:
+* 
+* object class_name
+* 
+* A brace:
+* 
+* brace {
+* 
+* Include file
+* 
+* use ~filename~
+* 
+* Make a functoin(no args)
+* 
+* funcdef function_name(*: _)
+* 
+* MAke a function(as many args)
+* 
+* funcdef more(var: int, vartwo: double)
+*/
+
+std::vector<std::string> dotcppfile{};
+
+struct first {};
+struct last {};
+int indent = 0;
+bool using_namespace_std = false;
+
+namespace corresponding
+{
+	std::string use = "#include";
+	std::string inputoutput = "<iostream>";
+	std::string str = "<string>";
+	std::string vector = "<vector>";
+	std::string output = "std::cout ";
+	std::string oppositetrailing = "<<";
+}
+
+auto get_first_keyword(std::string str_)
+{
+	int j = 0;
+	for (auto i : str_)
+	{
+		if (i == ' ')
+		{
+			break;
+		}
+		// incrememt the index count.
+		j++;
+	}
+
+
+	// so that we don't overlow.
+	if (j > str_.size())
+	{
+		j = 0;
+	}
+
+	// so now we have the index of the one past the last element of the key word.
+	// so we just now need to get the string out of that.
+
+	std::string keyword = str_.substr(0, j);
+
+	// so now we can just return the key word.
+	// we will not check if is a keyword first though.
+	// we will leave that to the user that called it.
+
+	return keyword;
+
+}
+
+auto is_keyword(std::string s)
+{
+	if (s == "use")
+	{
+		return true;
+	}
+	if (s == "funcdef")
+	{
+		return true;
+	}
+	if (s == "brace")
+	{
+		return true;
+	}
+	if (s == "output")
+	{
+		return true;
+	}
+	if (s == "object")
+	{
+		return true;
+	}
+	if (s == "funcdefmore")
+	{
+		return true;
+	}
+	if (s == "function_call")
+	{
+		return true;
+	}
+	if (s == "apply")
+	{
+		return true;
+	}
+	if (s == "body")
+	{
+		return true;
+	}
+	if (s == "vardef")
+	{
+		return true;
+	}
+	else
+	{
+		// FIXME: Add more key words and then the base case for something that isn't a keyword
+		// like a compiler error message.
+
+		return false;
+	}
+}
+
+struct exception
+{
+	std::string message{};
+
+	auto what() { return message; }
+};
+
+template<char c>
+auto has(std::string str_, first)
+{
+	if (str_[0] == c)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+template<char c>
+auto has(std::string str_, last)
+{
+	auto x = str_[str_.size() - 1];
+	if (x == c)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+auto amount_str(int num)
+{
+	std::string s_{};
+	for (int i = 0; i < num; i++)
+	{
+		s_ += " ";
+	}
+	return s_;
+}
+
+auto amount_str(char c, int num)
+{
+	std::string s_{};
+	for (int i = 0; i < num; i++)
+	{
+		s_ += std::to_string(c);
+	}
+}
+
+auto parse(std::string str_)
+{
+	auto possible_keyword = get_first_keyword(str_);
+	bool b = is_keyword(possible_keyword);
+	if (!b)
+	{
+		//throw exception{ "Error: '" + possible_keyword + "' is not a keyword." };
+		if (possible_keyword == "\n")
+		{
+			dotcppfile.push_back("\n");
+		}
+		std::string current_indent = amount_str(indent);
+		dotcppfile.push_back(current_indent + str_);
+	}
+	else
+	{
+		// A valid keyword has been entered, so we are going to use it, and get the rest of the
+		// expression.
+
+		if (possible_keyword == "use")
+		{
+			// It will be five because use: 3 + space:\/ 1 + current_character: 1
+			std::string next_expression = str_.substr(4, str_.size()); // substring from j to the end of the string.
+			bool a = has < '~' >(next_expression, first{}) && has<'~'>(next_expression, last{});
+			bool b = (next_expression[0] == '~') && (next_expression[next_expression.size() - 1] == '~');
+			if (!a && !b)
+			{
+				throw exception{ "Error: Missing '~' before and/or after the include file." };
+			}
+			else
+			{
+				// The user has entered the right syntax, so we carry on:
+				if (next_expression == "~inputoutput~")
+				{
+					std::string current_indent = amount_str(indent);
+					dotcppfile.push_back(current_indent + corresponding::use + corresponding::inputoutput);
+				}
+				if (next_expression == "~str~")
+				{
+					std::string current_indent = amount_str(indent);
+					dotcppfile.push_back(current_indent + corresponding::use + corresponding::str);
+				}
+				if (next_expression == "~vector~")
+				{
+					std::string current_indent = amount_str(indent);
+					dotcppfile.push_back(current_indent + corresponding::use + corresponding::vector);
+				}
+			}
+		} // if possible_keyword == "use"
+		else if (possible_keyword == "funcdef")
+		{
+			// Needs implementing.
+			// so with funcdef we will need to put it before every function definition like this:
+			// funcdef function_name(a: int) -> int
+			std::string current_string_o = amount_str(indent);
+			std::string next_expression = str_.substr(8, str_.size()); // substring from j to the end of the string.
+			current_string_o += "auto ";
+			std::string function_name{};
+			int IndexOfFindingFunctionName = 0;
+			for (int i = 0; i < next_expression.size(); i++)
+			{
+				if (next_expression[i] == '(')
+				{
+					function_name = next_expression.substr(0, i);
+					IndexOfFindingFunctionName = i;
+				}
+			}
+			current_string_o += function_name;
+			current_string_o += "(";
+			if (next_expression[IndexOfFindingFunctionName + 1] == '*')
+			{
+				// so this is the synatax for a void function:
+				// funcdef function_name(*: _)
+
+				if (next_expression.substr((IndexOfFindingFunctionName + 4), next_expression.size()) == "void")
+				{
+					next_expression = next_expression.substr(IndexOfFindingFunctionName + 9, next_expression.size());
+				}
+				else if (next_expression.substr((IndexOfFindingFunctionName + 4), next_expression.size()) == "_")
+				{
+					next_expression = next_expression.substr(IndexOfFindingFunctionName + 6);
+				}
+				current_string_o += ")";
+			}
+			else
+			{
+				// we have got a variable with a name. A variable will have the following syntax:
+				// var: _  or var: auto
+
+				int index_of_start_name = IndexOfFindingFunctionName;
+				int name_final_index = 0;
+				std::string current_str_to_deal = next_expression.substr(index_of_start_name, next_expression.size());
+				// for(int i = 0; i < curren)
+				for (int i = 0; i < current_str_to_deal.size(); i++)
+				{
+					if (current_str_to_deal[i] == ':')
+					{
+						name_final_index = i;
+					}
+				}
+				std::string name = current_str_to_deal.substr(1, name_final_index - 1);
+				if (current_str_to_deal.substr(name_final_index, name_final_index + 2) == " _")
+				{
+					std::string s = "auto " + name;
+					current_string_o += s;
+				}
+				else
+				{
+					// we have got to find the name of the type so we will just find the next ',' or ')' character
+					int last_type_index = 0;
+
+					for (int i = name_final_index; i < current_str_to_deal.size(); i++)
+					{
+						if (current_str_to_deal[i] == ')')
+						{
+							last_type_index = i - 1;
+						}
+					}
+					std::string type{};
+					type = current_str_to_deal.substr(name_final_index + 2, last_type_index - 1);
+					bool has_brace = false;
+					for (int i = 0; i < type.size(); i++)
+					{
+						if (type[i] == '(' || type[i] == ')')
+						{
+							has_brace = true;
+						}
+					}
+
+					bool has_brace_two = false;
+					for (int i = 0; i < type.size(); i++)
+					{
+						if (name[i] == '(' || name[i] == ')')
+						{
+							has_brace = true;
+						}
+					}
+					if (has_brace)
+					{
+						type = type.substr(0, type.size() - 1);
+					}
+					else if (!has_brace)
+					{
+						type = type.substr(0, type.size());
+					}
+					if (has_brace_two)
+					{
+						name = name.substr(0, name.size() - 1);
+					}
+					else if (!has_brace)
+					{
+						name = name.substr(0, name.size());
+					}
+					std::string s{ type + " " + name + ")" };
+					current_string_o += s;
+				}
+			}
+			dotcppfile.push_back(current_string_o);
+		} // else if(possible_keyword == "funcdef")
+		else if (possible_keyword == "object")
+		{
+		    std::string current_string_o = amount_str(indent);
+			std::string bit_after_object = str_.substr(6, str_.size());
+			// so we know have OBJECTNAME
+			current_string_o += "struct";
+			// we are going to have all things public by default.
+			std::string object_name = bit_after_object;
+			current_string_o += object_name;
+			dotcppfile.push_back(current_string_o);
+        }
+		else if (possible_keyword == "brace")
+		{
+			std::string next_expression = str_.substr(6, str_.size()); // substring from j to the end of the string.
+			if (next_expression == "{")
+			{
+				std::string current_indent = amount_str(indent);
+				indent += 4;
+				dotcppfile.push_back(current_indent + next_expression);
+			}
+			else if (next_expression == "}")
+			{
+				indent -= 4;
+				std::string current_indent = amount_str(indent);
+				dotcppfile.push_back(current_indent + next_expression);
+			}
+			else
+			{
+				// else the user has used an invalid key word and will now have to
+				// catch an exception or just abort the program.
+				throw exception{ "You have used the brace keyword in a wrong context" };
+			}
+		} // else if(possible_keyword == "brace"
+		else if (possible_keyword == "output")
+		{
+			std::string next_expression = str_.substr(7, str_.size());
+			if (next_expression.find("<-") != -1)
+			{
+				// so we are doing output <- something;
+				for (int i = 0; i < next_expression.size(); i++)
+				{
+					if (next_expression[i] == '<' && next_expression[i + 1] == '-')
+					{
+						std::string current_indent = amount_str(indent);
+						std::string output_thing = next_expression.substr(next_expression.find("<-") + 3, next_expression.size());
+						std::string output_s{};
+						if (using_namespace_std)
+						{
+							output_s = "cout ";
+						}
+						else
+						{
+							output_s = corresponding::output;
+						}
+						dotcppfile.push_back(current_indent + output_s + corresponding::oppositetrailing + " " + output_thing);
+					}
+				}
+			}
+		} // else if(possible_key_word == "output")
+		else if (possible_keyword == "funcdefmore")
+		{
+		    // we are now parsing multiple parameters use the keyword funcdefmore.
+			// the syntax will look like this:
+			// funcdefmore function_name(first: int, second: char, third: bool, ...)
+			std::string cppfile_waiting = amount_str(indent);
+			std::string rest_of_function = str_.substr(11, str_.size());
+			cppfile_waiting += "auto";
+			int last_index_of_name = 0;
+			for (int i = 0; i < rest_of_function.size(); i++)
+			{
+				if (rest_of_function[i] == '(')
+				{
+					last_index_of_name = i;
+					break;
+				}
+			}
+			bool last_parameter_reached = false;
+			std::string name = rest_of_function.substr(0, last_index_of_name);
+			cppfile_waiting += name;
+			cppfile_waiting += "(";
+			std::string parameters = rest_of_function.substr(last_index_of_name + 1, rest_of_function.size() - 1);
+			int last_reached_index = 0;
+
+			while (last_parameter_reached == false)
+			{
+				// so now we just have variablename: type, variablename2: type, variablename3: type etc.
+				// we will parse then one by one and then either delegate to another function, or in a while loop
+				int index_of_variable_name = 0;
+				for (int i = last_reached_index + 2; i < parameters.size(); i++)
+				{
+					if (parameters[i] == ',')
+					{
+						index_of_variable_name = i;
+						break;
+					}
+					if (parameters[i] == ')')
+					{
+						index_of_variable_name = i - 1;
+						last_parameter_reached = true;
+						break;
+					}
+				}
+				// so now we have the last index of the variable, for instance it could be:
+				// s: int
+				std::string variable{};
+				if (last_parameter_reached == true)
+				{
+					variable = parameters.substr(last_reached_index + 2, index_of_variable_name - 3);
+				}
+				if (last_parameter_reached == false)
+				{
+					variable = parameters.substr(last_reached_index, index_of_variable_name);
+				}
+				last_reached_index = index_of_variable_name;
+				// int temporary_index = index_of_variable_name
+				// now assign this at the end.
+				std::string name{};
+				std::string type{};
+				int last_name_index = 0;
+				for (int i = 0; i < variable.size(); i++)
+				{
+					if (variable[i] == ':')
+					{
+						last_name_index = i;
+						break;
+					}
+				}
+				name = variable.substr(0, last_name_index);
+				if (last_parameter_reached == false)
+				{
+					type = variable.substr(last_name_index + 2, variable.size());
+				}
+				else if (last_parameter_reached == true)
+				{
+					type = variable.substr(last_name_index + 2, variable.size() - 1);
+				}
+				last_name_index -= 1;
+
+				cppfile_waiting += (type + " " + name);
+				if (last_parameter_reached == true)
+				{
+					//cppfile_waiting.substr(0, cppfile_waiting.size() - 3);
+					cppfile_waiting += ")";
+				}
+				else
+				{
+					cppfile_waiting += ", ";
+				}
+			} // while(last_parameter_reached == false)
+			dotcppfile.push_back(cppfile_waiting);
+		} // else if(possible_keyword == "funcdefmore")
+		else if (possible_keyword == "function_call")
+		{
+		// this is the syntax for a function call:
+		// function_name function_name<parameters>!;
+		std::string cpp_file_waiting = amount_str(indent);
+		std::string rest_of_function_call = str_.substr(14, str_.size());
+		int end_index_function_name = 0;
+		for (int i = 0; i < rest_of_function_call.size(); i++)
+		{
+			if (rest_of_function_call[i] == '<')
+			{
+				end_index_function_name = i;
+				break;
+			}
+		}
+		std::string function_name = rest_of_function_call.substr(0, end_index_function_name);
+		cpp_file_waiting += function_name;
+		std::string rest_func = rest_of_function_call.substr(end_index_function_name, rest_of_function_call.size());
+		if (rest_func[0] == '<' && rest_func[1] == '>' && rest_func[2] == '!' && rest_func[3] == ';')
+		{
+			cpp_file_waiting += "();";
+		}
+
+		dotcppfile.push_back(cpp_file_waiting);
+		} // else if(possible_keyword == "function_call
+		else if (possible_keyword == "apply")
+		{
+		std::string cpp_file_waiting = amount_str(indent);
+		cpp_file_waiting += "using namespace ";
+		std::string namespace_name = str_.substr(6, str_.size());
+		if (namespace_name == "standard")
+		{
+			namespace_name = "std";
+		}
+		cpp_file_waiting += namespace_name;
+		if (namespace_name == "std")
+		{
+			using_namespace_std = true;
+		}
+		dotcppfile.push_back(cpp_file_waiting);
+		}
+		else if (possible_keyword == "body")
+		{
+		  // a body is a namespace.
+		  std::string namespace_name = str_.substr(5, str_.size());
+		  std::string cpp_waiting = amount_str(indent);
+		  cpp_waiting += ("namespace " + namespace_name);
+		  dotcppfile.push_back(cpp_waiting);
+		}
+		else if (possible_keyword == "vardef")
+		{
+		    // so we are making a variable, it is really simple.
+			std::string to_add_cpp = amount_str(indent);
+			to_add_cpp += "auto ";
+			std::string name_of_var{};
+			int index_of_var_name_last{};
+			for (int i = 7; i < str_.size(); i++)
+			{
+				if (str_[i] == '{')
+				{
+					index_of_var_name_last = i;
+				}
+			}
+			std::string rest = str_.substr(7, str_.size());
+			index_of_var_name_last -= 7;
+			std::string var_name = rest.substr(0, index_of_var_name_last);
+			to_add_cpp += var_name;
+			std::string value = rest.substr(index_of_var_name_last, rest.size());
+			to_add_cpp += value;
+			dotcppfile.push_back(to_add_cpp);
+		}
+	}
+}
+
+auto get_input()
+{
+	std::string s{};
+	int j = 1;
+	while (s != "End")
+	{
+		std::cout << "|" << j << "|    ";
+		std::getline(std::cin, s);
+		parse(s);
+		j++;
+	}
+	std::cout << "\n";
+}
+
+int main()
+{
+
+	get_input();
+
+	/*
+	parse("use ~inputoutput~");
+	parse("use ~str~");
+	parse("use ~vector~");
+	parse("int main()");
+	parse("brace {");
+	parse("output <- 42;");
+	parse("brace }");
+	parse("funcdef function_name(type: int)");
+	parse("brace {");
+	parse("brace }");
+	parse("funcdef function_name(avariablename: bool");
+	parse("brace {");
+	parse("brace }");
+	parse("struct a_struct");
+	parse("brace {");
+	parse("funcdef function(parameter: int)");
+	parse("brace {");
+	parse("output <- 42;");
+	parse("brace }");
+	parse("brace }");
+	parse("funcdef function_definition(*: _)");
+	parse("brace {");
+	parse("output <- 'c';");
+	parse("brace }");
+	parse("object object_name");
+	parse("brace {");
+	parse("brace }");
+	parse("funcdef main(*: _)");
+	parse("brace {");
+	parse("brace {");
+	parse("function_call function_definition<>!;");
+	parse("brace }");
+	parse("brace }");
+	*/
+	// FIME: outputs an extra bracket when not needed.
+	//parse("funcdefmore function_name(first: bool, second: int)");
+	//parse("brace {");
+	//parse("brace }");
+
+	int j = 1;
+	for (auto _ : dotcppfile)
+	{
+		if (j < 10)
+			std::cout << "|" << j << " |    " << _ << "\n";
+
+		else if (j < 100)
+			std::cout << "|" << j << "|    " << _ << "\n";
+		else
+			std::cout << "|" << j << "|   " << _ << "\n";
+		j++;
+
+	}
+}
